9c059810628c77053ed3288a95afd50ff026c84b
Jeffery Ye <s.jeffery.ye@gmail.com>
2025-10-18 16:43:26 -0700

    testing aws connection

--- server/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Route blueprints for the Lesson Scheduler server."""
-
-from .student import student_blueprint
-from .teacher import teacher_blueprint
-
-__all__ = ["student_blueprint", "teacher_blueprint"]
